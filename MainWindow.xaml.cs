4b961a0417040cd1c91b345ca4ca8a8a548c19b0
ooiyiheng <ooihengheng@gmail.com>
2023-05-01 17:27:41 +0800

    完成百分比程序

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace Calculator_project
         }
         float firstNumber, secondNumber; // firstNumber 儲存第一個數字，secondNumber 儲存第二個數字
         int operators = -1; // 記錄選擇哪一種運算符號？0:加、1:減、2:乘、3:除、-1:重新設定
+        float finalResults = 0f; //宣告最後計算結果變數
 
         private void btn_one_Click(object sender, RoutedEventArgs e)
         {
@@ -140,9 +141,13 @@ namespace Calculator_project
         {
             firstNumber = Convert.ToSingle(txt_Number.Text);
             txt_Number.Text = "0";
-            operators = 3; //選擇「除」號
+            operators = 3; //選擇「減」號
+        } 
+        private void btn_Percentage_Click(object sender, RoutedEventArgs e)
+        {
+            finalResults = finalResults / 10000;
+            txt_Number.Text = string.Format("{0:P2}", finalResults);
         }
-
         private void btn_dot_Click(object sender, RoutedEventArgs e)
         {
             // 確認輸入文字框中完全沒有小數點
@@ -157,10 +162,8 @@ namespace Calculator_project
             secondNumber = 0f;
             operators = -1;
         }
-
-        private void btn_equal_Click(object sender, RoutedEventArgs e)
+        public void btn_equal_Click(object sender, RoutedEventArgs e)
         {
-            float finalResults = 0f; //宣告最後計算結果變數
             secondNumber = Convert.ToSingle(txt_Number.Text); //將輸入文字框轉換成浮點數，存入第二個數字的全域變數
 
             //依照四則運算符號的選擇，進行加減乘除
@@ -177,6 +180,9 @@ namespace Calculator_project
                     break;
                 case 3:
                     finalResults = firstNumber / secondNumber;
+                    break;
+                case 4:
+                    txt_Number.Text = string.Format("{0:P2}", finalResults);
                     break;
             }
             txt_Number.Text = string.Format("{0:0.##########}", finalResults); //在輸入文字框中，顯示最後計算結果，並且轉換成格式化的字串內容
